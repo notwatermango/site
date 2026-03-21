3d628cba2756ae30b0a900fc415a9c5621acdb46
danzelmba15 <notwatermango@gmail.com>
2026-03-22 03:22:42 +0700

    chore: allow dev origin `192.168.*.*`

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
   /* config options here */
+  allowedDevOrigins: ["192.168.*.*", "localhost"],
 };
 
 export default nextConfig;
